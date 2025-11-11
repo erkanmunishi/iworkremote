78d3233fe505f1f838302460cffd4d7a05172f99
Erkan Munishi <erkan@consultdemy.com>
2025-11-10 20:23:29 -0500

    fix: import NewsletterForm as default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { NewsletterForm } from "@/components/newsletter-form";
+import NewsletterForm from "@/components/newsletter-form";
 
 export const metadata = { title: "Home" };
 
